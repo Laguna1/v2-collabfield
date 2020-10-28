978f9fccd555a58a907489fb79a6d85bccb605de
Laguna1 <lagunazoomarket@gmail.com>
2020-10-28 22:38:45 +0200

    - Add the bootstrap layout to the home page - Add CSS to make home page layout's stylistic and responsive design changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
+//= require bootstrap/dropdown
 //= require bootstrap-sprockets
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
