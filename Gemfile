393ce5a7990b0c6c6742a37ae89f979e0dad4b1b
Laguna1 <lagunazoomarket@gmail.com>
2020-10-28 18:32:30 +0200

    Generate devise views, modify sign in form and add the bootstrap_form gem.

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'puma', '~> 3.11'
 # Use SCSS for stylesheets
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '~> 5.0'
+gem 'bootstrap_form'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'login', to: 'devise/sessions#new'
+  get 'signup', to: 'devise/registrations#new'
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
