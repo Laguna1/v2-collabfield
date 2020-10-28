ff7f776da0c93ab0a6b240244f423827e383162c
Laguna1 <lagunazoomarket@gmail.com>
2020-10-28 18:49:59 +0200

    Delete everything from the signup page, except the form. Convert form into a bootstrap form. Add an additional name field

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     get 'login', to: 'devise/sessions#new'
   end
 
-  get 'signup', to: 'devise/registrations#new'
+  devise_scope :user do
+    get 'signup', to: 'devise/registrations#new'
+  end
+  
   root to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
