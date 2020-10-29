84df3ef0f6d95ce867f31cbc3fce9c20e2c95d0c
Laguna1 <lagunazoomarket@gmail.com>
2020-10-29 23:52:27 +0200

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+    def redirect_if_not_signed_in
+        redirect_to root_path if !user_signed_in?
+      end
+      
+      def redirect_if_signed_in
+        redirect_to root_path if user_signed_in?
+      end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class PostsController < ApplicationController
+  before_action :redirect_if_not_signed_in, only: [:new]
+  
   def index
     @posts = Post.limit(5)
   end
@@ -21,6 +23,21 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
+  def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save 
+      redirect_to post_path(@post) 
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def posts_for_branch(branch)
@@ -38,5 +55,10 @@ class PostsController < ApplicationController
       category: params[:category],
       branch: params[:action]
     }).call
+  end
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+                         .merge(user_id: current_user.id)
   end
 end
